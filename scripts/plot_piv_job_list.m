08b4bd545ed9578ebaef1948b1fcb9b6ac290d44
Matthew Giarra <matthew.giarra@gmail.com>
2016-12-06 13:25:08 -0500

    Added ability to plot specific pass and type of vectors (raw, validated, smoothed, final) to plot_piv_job_list.m

--- scripts/plot_piv_job_list.m
+++ scripts/plot_piv_job_list.m
@@ -1,7 +1,29 @@
-function plot_piv_job_list(JOBLIST)
+function plot_piv_job_list(JOBLIST, PASS_NUMBER, VECT_TO_PLOT)
+
+if nargin < 3
+    VECT_TO_PLOT = 'final';
+end
+
+if nargin < 2
+    PASS_NUMBER = 0;
+end
+
+% Set pass number to 0 if an empty argument is passed.
+if isempty(PASS_NUMBER)
+    PASS_NUMBER = 0;
+end
+
+plot_final = ~isempty(regexpi(VECT_TO_PLOT, 'fi'));
+plot_validated = ~isempty(regexpi(VECT_TO_PLOT, 'va'));
+plot_smoothed = ~isempty(regexpi(VECT_TO_PLOT, 'sm'));
+plot_raw = ~isempty(regexpi(VECT_TO_PLOT, 'raw'));
 
 % Vector scale
-vect_scale = 4;
+vect_scale = 2;
+
+% Vector skip
+skip_x = 4;
+skip_y = 4;
 
 % Count the number of jobs
 num_jobs = length(JOBLIST);
@@ -26,25 +48,52 @@ for n = 1 : num_jobs
     file_name_plot = strrep(file_name, '_', '\_');
     
     % Load the output file
-    load(output_file_path);
+    loaded_jobfile = load(output_file_path);
     
-    % Only plot the raw, unsmoothed fields.
-    tx = JobFile.Processing(end).Results.Displacement.Raw.X(:, 1);
-    ty = JobFile.Processing(end).Results.Displacement.Raw.Y(:, 1);
+    % Loaded job file
+    JobFile_Loaded = loaded_jobfile.JobFile;
     
     % Measure the number of passes
-    num_passes = length(JobFile.Processing);
+    num_passes = length(JobFile_Loaded.Processing);
     
-    % Extract the grid
-    gx = JobFile.Processing(num_passes).Grid.Points.Full.X;
-    gy = JobFile.Processing(num_passes).Grid.Points.Full.Y;
+    % Figure out which pass to plot
+    if PASS_NUMBER == 0
+        pass_to_plot = num_passes;
+    else
+        % Figure out which pass to plot
+        pass_to_plot = min(PASS_NUMBER, num_passes);
+    end
+        
+    % Determine which displacement field to plot
+    if plot_raw
+        displacement_field = JobFile_Loaded.Processing(pass_to_plot).Results.Displacement.Raw;
+    elseif plot_validated
+        displacement_field = JobFile_Loaded.Processing(pass_to_plot).Results.Displacement.Validated;
+    elseif plot_smoothed
+        displacement_field = JobFile_Loaded.Processing(pass_to_plot).Results.Displacement.Smoothed;
+    elseif plot_final
+        displacement_field = JobFile_Loaded.Processing(pass_to_plot).Results.Displacement.Final;
+    end
+
+    % Read the displacement field
+    tx = displacement_field.X(:, 1);
+    ty = displacement_field.Y(:, 1);
+    
+    % Read the grid points.
+    gx = JobFile_Loaded.Processing(pass_to_plot).Grid.Points.Full.X;
+    gy = JobFile_Loaded.Processing(pass_to_plot).Grid.Points.Full.Y;
     
     % Count the grid points
     nx = length(unique(gx(:)));
     ny = length(unique(gy(:)));
     
+    % Reshape grid
+    gx_mat = reshape(gx, [ny, nx]);
+    gy_mat = reshape(gy, [ny, nx]);
+    
     % Reshape vectors
     tx_mat = reshape(tx, [ny, nx]);
+    ty_mat = reshape(ty, [ny, nx]);
     
     % Create a figure
     figure(n);
@@ -52,6 +101,11 @@ for n = 1 : num_jobs
     axis image;
     hold on;
     quiver(gx, gy, vect_scale * tx, vect_scale * ty, 0, '-k', 'linewidth', 1.5);
+    quiver(             gx_mat(1 : skip_y : end, 1 : skip_x : end), ...
+                        gy_mat(1 : skip_y : end, 1 : skip_x : end), ...
+           vect_scale * tx_mat(1 : skip_y : end, 1 : skip_x : end), ...
+           vect_scale * ty_mat(1 : skip_y : end, 1 : skip_x : end), ...
+           0, '-k', 'linewidth', 1.5);
 
     % Load the mask
     grid_mask = load_mask(JobFile, num_passes);
