33558ac0ae9ef036c215c1f6ddc0398014e104b4
Matthew Giarra <matthew.giarra@gmail.com>
2016-12-15 23:56:23 -0500

    Added a directory called 'tools' to hold codes that are not related specifically to image processing. Things like renaming batches of files (tools/renamer/renamer.m does precisely that).

---
+++ tools/renamer/rename_images.m
@@ -0,0 +1,16 @@
+
+FILEDIR = '/Users/matthewgiarra/Documents/School/VT/Research/Aether/piv_test_images/grasshopper/grasshopper_8/mng-1-163-C/proc/div';
+OLDBASENAME = 'mng-1-163-C_div_';
+NEWBASENAME = 'mng-1-163-C_div_';
+
+NDIGITS = 6;
+OLDTRAILER = '';
+NEWTRAILER = '';
+
+OLD_EXTENSION = '.tif';
+NEW_EXTENSION = '.tiff';
+START = 1;
+STOP = 99;
+
+
+renamer(FILEDIR, OLDBASENAME, NEWBASENAME, NDIGITS, OLDTRAILER, NEWTRAILER, OLD_EXTENSION, NEW_EXTENSION, START, STOP);
