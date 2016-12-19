0622e7d60acd6a0eb39cf201bf461adc7026cd9e
Matthew Giarra <matthew.giarra@gmail.com>
2016-12-19 11:53:57 -0500

    Updated read_ensemble_domain.m to have smarter defaults.

--- read_ensemble_domain.m
+++ read_ensemble_domain.m
@@ -5,8 +5,25 @@ if nargin < 2
     PASS_NUMBER = 1;
 end
 
-% Read the ensemble domain
-ensemble_domain_string = ...
-    lower(JOBFILE.Processing(PASS_NUMBER).Correlation.Ensemble.Domain);
+% Processing field
+proc_field = JOBFILE.Processing(PASS_NUMBER).Correlation;
+
+% Default to ensemble domain as "none"
+ensemble_domain_string = 'none';
+
+% Check if the ensemble field exists.
+if isfield(proc_field, 'Ensemble')
+   
+    % Extract the ensemble field.
+    ensemble_field = proc_field.Ensemble;
+    
+    % Check if the "Domain" field is specified.
+    % If not, default to spatial.
+    if isfield(ensemble_field, 'Domain');
+       
+        % Read the field.
+        ensemble_domain_string = lower(ensemble_field.Domain);
+    end
+end
 
 end
